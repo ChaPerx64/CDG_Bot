fd5ccd4c1c1afae2793409966e5ee948ec35de8c
@CheParx64 <chayan.par@gmail.com>
2023-08-02 17:29:30 +0200

    default empty message added

--- app/TMPers.py
+++ app/TMPers.py
@@ -4,8 +4,11 @@ from os.path import dirname, join
 
 
 def rf_text(f_name: str):
-    with open(join(dirname(__file__), 'custom_messages', f_name), encoding='utf-8') as t:
-        return t.read()
+    try:
+        with open(join(dirname(__file__), 'custom_messages', f_name), encoding='utf-8') as t:
+            return t.read()
+    except FileNotFoundError:
+        return ''
 
 
 def wf_text(f_name: str, text: str):
